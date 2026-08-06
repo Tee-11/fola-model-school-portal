59927a714a4309e05c720f5169c1be0d7b8c38cf
Tee-11 <idowutemitayo2011@gmail.com>
2026-08-06 01:19:16 +0100

    add all subjects

--- data.py
+++ data.py
@@ -3,7 +3,15 @@ STUDENTS= {
     "abolarinwasemilore_ss3_science": "FOLA002",
     "amosdavid_jss1_general": "FOLA003",
     "samtee_jss2_general": "FOLA004",
-    "temmysam_jss3_general": "FOLA005"
+    "temmysam_jss3_general": "FOLA005",
+    "balogunrayhan_ss1_science": "FOLA006",
+    "abdullahimubarak_ss1_art": "FOLA007",
+    "alimqusum_ss1_commercial": "FOLA008",
+    "oyebanjisamuel_ss2_science": "FOLA009",
+    "oshibotupelumi_ss2_art": "FOLA010",
+    "olaleyepeace_ss2_commercial": "FOLA011",
+    "jamesjoy_ss3_art": "FOLA012"
+    "olumoyewaabigail_ss3_commercial": "FOLA013"
 }
 
 NOTES= {
@@ -54,41 +62,105 @@ NOTES= {
     },
     "ss1": {
         "science": {
-            "Mathematics": "http://scribd.com/document/662823055/MathematicsLessonNotesSS1FirstTerm-Edudelight-com-1691072173306"
+            "Mathematics": "http://edudelight.com/lesson-notes-mathematics-ss1-first-term/",
+            "English Language": "http://edudelight.com/english-studies-lesson-note-for-ss-1-first-term/",
+            "Civic Education": "http://edudelight.com/lesson-note-on-civic-education-ss-1-first-term/",
+            "Agricultural Science": "http://edudelight.com/lesson-note-on-agricultural-science-ss-1-first-term/",
+            "Chemistry": "http://edudelight.com/lesson-note-on-chemistry-ss1-first-term/",
+            "Physics": "http://edudelight.com/lesson-note-on-physics-ss1-first-term/",
+            "Biology": "http://edudelight.com/lesson-note-on-biology-ss1-first-term/",
+            "Geography": "http://www.scribd.com/document/667726843/1ST-TERM-S1-GEOGRAPHY"
        },
 
        "art": {
-            "Mathematics": "http://scribd.com/document/662823055/MathematicsLessonNotesSS1FirstTerm-Edudelight-com-1691072173306"
+            "Mathematics": "http://edudelight.com/lesson-notes-mathematics-ss1-first-term/",
+            "English Language": "http://edudelight.com/english-studies-lesson-note-for-ss-1-first-term/",
+            "Civic Education": "http://edudelight.com/lesson-note-on-civic-education-ss-1-first-term/",
+            "Agricultural Science": "http://edudelight.com/lesson-note-on-agricultural-science-ss-1-first-term/",
+            "Government": "http://edudelight.com/government-lesson-note-ss1-first-term/",
+            "Economics": "http://edudelight.com/economics-lesson-note-for-ss1-first-term/",
+            "Literature": "http://edudelight.com/lesson-note-on-literature-in-english-ss1-first-term/",
+            "Christian Religious Studies": "http://edudelight.com/christian-religious-studies-crs-lesson-note-ss1-first-term/"
        },
 
        "commercial": {
-            "Mathematics": "http://scribd.com/document/662823055/MathematicsLessonNotesSS1FirstTerm-Edudelight-com-1691072173306"
+            "Mathematics": "http://edudelight.com/lesson-notes-mathematics-ss1-first-term/",
+            "English Language": "http://edudelight.com/english-studies-lesson-note-for-ss-1-first-term/",
+            "Civic Education": "http://edudelight.com/lesson-note-on-civic-education-ss-1-first-term/",
+            "Agricultural Science": "http://edudelight.com/lesson-note-on-agricultural-science-ss-1-first-term/",
+            "Government": "http://edudelight.com/government-lesson-note-ss1-first-term/",
+            "Economics": "http://edudelight.com/economics-lesson-note-for-ss1-first-term/",
+            "Commerce": "http://edudelight.com/commerce-lesson-note-for-ss1-first-term/",
+            "Financial Accounting": "http://edudelight.com/financial-accounting-lesson-note-ss1-first-term/"
        }
     },
     "ss2": {
        "science": {
-            "Mathematics": "http://scribd.com/document/606732548/SS2-Maths-1st-Term-Lesson-Note-pdf"
-       },
+            "Mathematics": "http://edudelight.com/mathematics-lesson-notes-ss2-first-term/",
+            "English Language": "http://edudelight.com/lesson-note-on-english-language-ss-2-first-term/",
+            "Civic Education": "http://edudelight.com/lesson-note-civic-education-ss-2-first-term/",
+            "Agricultural Science": "http://edudelight.com/agricultural-science-lesson-note-ss-2-first-term/",
+            "Chemistry": "http://edudelight.com/lesson-note-on-chemistry-ss2-first-term/",
+            "Physics": "http://edudelight.com/lesson-notes-on-physics-ss2-first-term/",
+            "Biology": "http://edudelight.com/biology-lesson-note-ss2-first-term/",
+            "Geography": "http://www.scribd.com/document/616522620/1st-Term-s2-Geography"
+    },
 
        "art": {
-            "Mathematics": "http://scribd.com/document/606732548/SS2-Maths-1st-Term-Lesson-Note-pdf"
+            "Mathematics": "http://edudelight.com/mathematics-lesson-notes-ss2-first-term/",
+            "English Language": "http://edudelight.com/lesson-note-on-english-language-ss-2-first-term/",
+            "Civic Education": "http://edudelight.com/lesson-note-civic-education-ss-2-first-term/",
+            "Agricultural Science": "http://edudelight.com/agricultural-science-lesson-note-ss-2-first-term/",
+            "Government": "http://edudelight.com/lesson-note-on-government-ss2-first-term/",
+            "Economics": "http://edudelight.com/economics-lesson-note-for-ss2-first-term/",
+            "Literature": "http://edudelight.com/lesson-note-on-literature-in-english-ss2-first-term/",
+            "Christian Religious Studies": "http://edudelight.com/lesson-notes-on-christian-religious-studies-crs-ss2-first-term/"
        },
 
        "commercial": {
-            "Mathematics": "http://scribd.com/document/606732548/SS2-Maths-1st-Term-Lesson-Note-pdf"
+            "Mathematics": "http://edudelight.com/mathematics-lesson-notes-ss2-first-term/",
+            "English Language": "http://edudelight.com/lesson-note-on-english-language-ss-2-first-term/",
+            "Civic Education": "http://edudelight.com/lesson-note-civic-education-ss-2-first-term/",
+            "Agricultural Science": "http://edudelight.com/agricultural-science-lesson-note-ss-2-first-term/",
+            "Government": "http://edudelight.com/lesson-note-on-government-ss2-first-term/",
+            "Economics": "http://edudelight.com/economics-lesson-note-for-ss2-first-term/",
+            "Commerce": "http://edudelight.com/commerce-lesson-note-for-ss2-first-term/",
+            "Financial Accounting": "http://edudelight.com/financial-accounting-lesson-note-ss2-first-term-2/"
        }
     },
     "ss3": {
        "science": {
-            "Mathematics": "http://scribd.com/document/753822955/SS3-Mathematics-E-note"
+            "Mathematics": "http://edudelight.com/mathematics-lesson-notes-ss3-first-term/",
+            "English Language": "http://edudelight.com/lesson-note-on-english-language-ss-3-first-term/",
+            "Civic Education": "http://edudelight.com/civic-education-lesson-note-ss-3-first-term/",
+            "Agricultural Science": "http://edudelight.com/agricultural-science-lesson-note-ss-3-first-term/",
+            "Chemistry": "http://edudelight.com/lesson-note-on-chemistry-ss3-first-term/",
+            "Physics": "http://edudelight.com/lesson-note-on-physics-ss3-first-term/",
+            "Biology": "http://edudelight.com/lesson-note-on-biology-ss3-first-term/",
+            "Geography": "http://www.scribd.com/document/667726836/1st-Term-s3-Geography",
+            "Economics": "http://edudelight.com/lesson-note-on-economics-ss3-first-term/"
        },
 
        "art": {
-            "Mathematics": "http://scribd.com/document/753822955/SS3-Mathematics-E-note"
+            "Mathematics": "http://edudelight.com/mathematics-lesson-notes-ss3-first-term/",
+            "English Language": "http://edudelight.com/lesson-note-on-english-language-ss-3-first-term/",
+            "Civic Education": "http://edudelight.com/civic-education-lesson-note-ss-3-first-term/",
+            "Agricultural Science": "http://edudelight.com/agricultural-science-lesson-note-ss-3-first-term/",
+            "Government": "http://edudelight.com/lesson-note-government-ss3-first-term/",
+            "Economics": "http://edudelight.com/lesson-note-on-economics-ss3-first-term/",
+            "Literature": "http://edudelight.com/lesson-note-on-literature-in-english-ss3-first-term/",
+            "Christian Religious Studies": "http://edudelight.com/lesson-notes-on-christian-religious-studies-crs-ss3-first-term/"
        },
 
        "commercial": {
-            "Mathematics": "http://scribd.com/document/753822955/SS3-Mathematics-E-note"
+            "Mathematics": "http://edudelight.com/mathematics-lesson-notes-ss3-first-term/",
+            "English Language": "http://edudelight.com/lesson-note-on-english-language-ss-3-first-term/",
+            "Civic Education": "http://edudelight.com/civic-education-lesson-note-ss-3-first-term/",
+            "Agricultural Science": "http://edudelight.com/agricultural-science-lesson-note-ss-3-first-term/",
+            "Government": "http://edudelight.com/lesson-note-government-ss3-first-term/",
+            "Economics": "http://edudelight.com/lesson-note-on-economics-ss3-first-term/",
+            "Commerce": "http://edudelight.com/commerce-lesson-note-for-ss3-first-term/",
+            "Financial Accounting": "http://edudelight.com/financial-accounting-lesson-note-ss3-first-term/"
        }
     }
 }
